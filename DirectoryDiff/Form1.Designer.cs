2a68a8bbc3f33b6305cfe3f5a9029c3c73dfd35b
agrant40 <adam.grant40@gmail.com>
2014-05-17 17:02:52 -0500

    Initial commit

---
+++ DirectoryDiff/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace DirectoryDiff
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse1 = new System.Windows.Forms.Button();
+            this.btnBrowse2 = new System.Windows.Forms.Button();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.btnDiff = new System.Windows.Forms.Button();
+            this.txtDir1 = new System.Windows.Forms.TextBox();
+            this.txtDir2 = new System.Windows.Forms.TextBox();
+            this.btnSimiliar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBrowse1
+            // 
+            this.btnBrowse1.Location = new System.Drawing.Point(353, 12);
+            this.btnBrowse1.Name = "btnBrowse1";
+            this.btnBrowse1.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse1.TabIndex = 2;
+            this.btnBrowse1.Text = "Browse";
+            this.btnBrowse1.UseVisualStyleBackColor = true;
+            this.btnBrowse1.Click += new System.EventHandler(this.btnBrowse1_Click);
+            // 
+            // btnBrowse2
+            // 
+            this.btnBrowse2.Location = new System.Drawing.Point(353, 56);
+            this.btnBrowse2.Name = "btnBrowse2";
+            this.btnBrowse2.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse2.TabIndex = 3;
+            this.btnBrowse2.Text = "Browse";
+            this.btnBrowse2.UseVisualStyleBackColor = true;
+            this.btnBrowse2.Click += new System.EventHandler(this.btnBrowse2_Click);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(12, 151);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(451, 199);
+            this.lstFiles.TabIndex = 4;
+            // 
+            // btnDiff
+            // 
+            this.btnDiff.Location = new System.Drawing.Point(255, 96);
+            this.btnDiff.Name = "btnDiff";
+            this.btnDiff.Size = new System.Drawing.Size(173, 23);
+            this.btnDiff.TabIndex = 5;
+            this.btnDiff.Text = "List Differences";
+            this.btnDiff.UseVisualStyleBackColor = true;
+            this.btnDiff.Click += new System.EventHandler(this.btnDiff_Click);
+            // 
+            // txtDir1
+            // 
+            this.txtDir1.Location = new System.Drawing.Point(42, 15);
+            this.txtDir1.Name = "txtDir1";
+            this.txtDir1.Size = new System.Drawing.Size(305, 20);
+            this.txtDir1.TabIndex = 6;
+            // 
+            // txtDir2
+            // 
+            this.txtDir2.Location = new System.Drawing.Point(42, 59);
+            this.txtDir2.Name = "txtDir2";
+            this.txtDir2.Size = new System.Drawing.Size(305, 20);
+            this.txtDir2.TabIndex = 7;
+            // 
+            // btnSimiliar
+            // 
+            this.btnSimiliar.Location = new System.Drawing.Point(42, 96);
+            this.btnSimiliar.Name = "btnSimiliar";
+            this.btnSimiliar.Size = new System.Drawing.Size(173, 23);
+            this.btnSimiliar.TabIndex = 8;
+            this.btnSimiliar.Text = "List Files in Both";
+            this.btnSimiliar.UseVisualStyleBackColor = true;
+            this.btnSimiliar.Click += new System.EventHandler(this.btnSimiliar_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 362);
+            this.Controls.Add(this.btnSimiliar);
+            this.Controls.Add(this.txtDir2);
+            this.Controls.Add(this.txtDir1);
+            this.Controls.Add(this.btnDiff);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.btnBrowse2);
+            this.Controls.Add(this.btnBrowse1);
+            this.Name = "frmMain";
+            this.Text = "DirectoryDiff";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse1;
+        private System.Windows.Forms.Button btnBrowse2;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Button btnDiff;
+        private System.Windows.Forms.TextBox txtDir1;
+        private System.Windows.Forms.TextBox txtDir2;
+        private System.Windows.Forms.Button btnSimiliar;
+    }
+}
+
---
+++ DirectoryDiff/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DirectoryDiff
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse1_Click(object sender, EventArgs e)
+        {
+            string folderPath = "";
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+            }
+
+            txtDir1.Text = folderPath;
+        }
+
+        private void btnBrowse2_Click(object sender, EventArgs e)
+        {
+            string folderPath = "";
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+            }
+
+            txtDir2.Text = folderPath;
+        }
+
+        private void btnDiff_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(txtDir1.Text) && !String.IsNullOrEmpty(txtDir2.Text))
+            {
+                string pathA = txtDir1.Text;
+                string pathB = txtDir2.Text;
+
+                System.IO.DirectoryInfo dir1 = new System.IO.DirectoryInfo(pathA);
+                System.IO.DirectoryInfo dir2 = new System.IO.DirectoryInfo(pathB);
+
+                // Take a snapshot of the file system.
+                IEnumerable<System.IO.FileInfo> list1 = dir1.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+                IEnumerable<System.IO.FileInfo> list2 = dir2.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+
+                //A custom file comparer defined in FileCompare.cs
+                FileCompare myFileCompare = new FileCompare();
+
+                // This query determines whether the two folders contain 
+                // identical file lists, based on the custom file comparer 
+                // that is defined in the FileCompare class. 
+                // The query executes immediately because it returns a bool. 
+                bool areIdentical = list1.SequenceEqual(list2, myFileCompare);
+
+                if (areIdentical == true)
+                {
+                    lstFiles.Items.Add("the two folders are the same");
+                }
+                else
+                {
+                    lstFiles.Items.Add("The two folders are not the same");
+                }
+
+                // Find the set difference between the two folders. 
+                // Start with Folder 1
+                var queryList1Only = (from file in list1
+                                      select file).Except(list2, myFileCompare);
+
+                lstFiles.Items.Add("The following files are in Directory 1 but not Directory 2:");
+                foreach (var v in queryList1Only)
+                {
+                    lstFiles.Items.Add(v.FullName);
+                }
+
+                // Then Folder 2
+                var queryList2Only = (from file in list2
+                                      select file).Except(list1, myFileCompare);
+
+                lstFiles.Items.Add("The following files are in Directory 2 but not in Directory 1:");
+                foreach(var v in queryList2Only)
+                {
+                    lstFiles.Items.Add(v.FullName);
+                }
+            }
+        }
+
+        private void btnSimiliar_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtDir1.Text) && !String.IsNullOrEmpty(txtDir2.Text))
+            {
+                string pathA = txtDir1.Text;
+                string pathB = txtDir2.Text;
+
+                System.IO.DirectoryInfo dir1 = new System.IO.DirectoryInfo(pathA);
+                System.IO.DirectoryInfo dir2 = new System.IO.DirectoryInfo(pathB);
+
+                // Take a snapshot of the file system.
+                IEnumerable<System.IO.FileInfo> list1 = dir1.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+                IEnumerable<System.IO.FileInfo> list2 = dir2.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+
+                //A custom file comparer defined in FileCompare.cs
+                FileCompare myFileCompare = new FileCompare();
+
+                // This query determines whether the two folders contain 
+                // identical file lists, based on the custom file comparer 
+                // that is defined in the FileCompare class. 
+                // The query executes immediately because it returns a bool. 
+                bool areIdentical = list1.SequenceEqual(list2, myFileCompare);
+
+                if (areIdentical == true)
+                {
+                    lstFiles.Items.Add("the two folders are the same");
+                }
+                else
+                {
+                    lstFiles.Items.Add("The two folders are not the same");
+                }
+
+                // Find the common files. It produces a sequence and doesn't  
+                // execute until the foreach statement. 
+                var queryCommonFiles = list1.Intersect(list2, myFileCompare);
+
+                if (queryCommonFiles.Count() > 0)
+                {
+                    lstFiles.Items.Add("The following files are in both folders:");
+                    foreach (var v in queryCommonFiles)
+                    {
+                        lstFiles.Items.Add(v.FullName); //shows which items end up in result list
+                    }
+                }
+                else
+                {
+                    lstFiles.Items.Add("There are no common files in the two folders.");
+                }
+            }
+        }
+    }
+}
